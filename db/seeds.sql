9b391aea485c7eebf40bc37452ad3f3097200913
Nicholas Golden <npg311@gmail.com>
2019-07-29 14:20:31 -0500

    basic burger app template

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Large Marge', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hillbilly', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
